51b2202fc0cd772d6fe0d87e79e6f80af5f7a1b2
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2022-01-17 17:28:29 +0530

    first commit

---
+++ engine.js
@@ -0,0 +1,328 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = false;
+
+class KeyboardState {
+    constructor() {
+        this.keys = {};
+
+        function keyUpCallback(e) {
+            e.preventDefault();
+            this.keys[e.code] = false;
+        }
+
+        function keyDownCallback(e) {
+            e.preventDefault();
+            this.keys[e.code] = true;
+        }
+
+        keyUpCallback = keyUpCallback.bind(this);
+        keyDownCallback = keyDownCallback.bind(this);
+
+        window.addEventListener("keydown", keyDownCallback);
+        window.addEventListener("keyup", keyUpCallback);
+    }
+
+    isKeyDown(key) {
+        return this.keys[key];
+    }
+
+    isKeyUp(key) {
+        return !this.keys[key];
+    }
+}
+
+class Vector2 {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static add(v1, v2) {
+        return new Vector2(v1.x + v2.x, v1.y + v2.y);
+    }
+
+    static subtract(v1, v2) {
+        return new Vector2(v1.x - v2.x, v1.y - v2.y);
+    }
+
+    static multiply(v1, v2) {
+        return new Vector2(v1.x * v2.x, v1.y * v2.y);
+    }
+
+    static divide(v1, v2) {
+        return new Vector2(v1.x / v2.x, v1.y / v2.y);
+    }
+
+    static dot(v1, v2) {
+        return v1.x * v2.x + v1.y * v2.y;
+    }
+
+    static cross(v1, v2) {
+        return v1.x * v2.y - v1.y * v2.x;
+    }
+
+    static distance(v1, v2) {
+        return Math.sqrt(Math.pow(v1.x - v2.x, 2) + Math.pow(v1.y - v2.y, 2));
+    }
+
+    static angleBetween(v1, v2) {
+        return Math.acos(Vector2.dot(v1, v2) / (v1.magnitude() * v2.magnitude()));
+    }
+
+    static angleBetweenDegrees(v1, v2) {
+        return Vector2.angleBetween(v1, v2) * 180 / Math.PI;
+    }
+
+    static normalize(v1) {
+        return Vector2.divide(v1, v1.magnitude());
+    }
+
+    static zero() {
+        return new Vector2(0, 0);
+    }
+
+    static one() {
+        return new Vector2(1, 1);
+    }
+}
+
+class Point {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(p1, p2) {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    }
+    static zero() {
+        return new Point(0, 0);
+    }
+
+    static one() {
+        return new Point(1, 1);
+    }
+}
+
+class Rectangle {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    static contains(rect, point) {
+        return point.x >= rect.x && point.x <= rect.x + rect.width && point.y >= rect.y && point.y <= rect.y + rect.height;
+    }
+
+    static intersects(rect1, rect2) {
+        return rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y;
+    }
+}
+
+class Primitives2D {
+    static drawLine(x1, y1, x2, y2, color = '#000000', lineWidth = 1) {
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
+        ctx.stroke();
+        ctx.closePath();
+
+        ctx.beginPath();
+    }
+
+    static drawRectangle(x, y, width, height, color = '#000000', lineWidth = 1) {
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.rect(x, y, width, height);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
+        ctx.stroke();
+        ctx.closePath();
+
+        ctx.beginPath();
+    }
+
+    static fillRectangle(x, y, width, height, color = '#000000') {
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, width, height);
+        ctx.closePath();
+
+        ctx.beginPath();
+    }
+
+    static drawCircle(x, y, radius, color = '#000000', lineWidth = 1) {
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
+        ctx.stroke();
+        ctx.closePath();
+
+        ctx.beginPath();
+    }
+
+    static fillCircle(x, y, radius, color = '#000000') {
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.beginPath();
+    }
+
+    static drawText(text, x, y, color = '#000000', font = '10px Arial') {
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        ctx.font = font;
+        ctx.fillText(text, x, y);
+        ctx.closePath();
+
+        ctx.beginPath();
+    }
+}
+
+class Animation {
+    constructor(texture, frameWidth, frameRate = 4, looping = false, scale = 1, nextAnimation = '') {
+        this.texture = texture;
+        this.frameWidth = frameWidth;
+        this.frameRate = frameRate;
+        this.looping = looping;
+
+        this.currentFrame = 0;
+
+        this.time = 0;
+        this.scale = scale;
+
+        this.finishedPlaying = false;
+        this.nextAnimation = nextAnimation;
+    }
+
+    play() {
+        this.finishedPlaying = false;
+        this.currentFrame = 0;
+    }
+
+    update() {
+        this.time++;
+
+        if (this.time > this.frameRate) {
+            this.time = 0;
+            this.currentFrame++;
+
+            if (this.currentFrame > this.texture.width / this.frameWidth - 1) {
+                if (this.looping) {
+                    this.currentFrame = 0;
+                }
+                else {
+                    this.currentFrame = this.texture.width / this.frameWidth - 1;
+                    this.finishedPlaying = true;
+                }
+            }
+        }
+    }
+
+    draw(x, y, flipped = false, origin = Vector2.zero) {
+        ctx.save();
+
+        x = flipped ? (x + origin.x * this.scale / 2 ) * -1 : x - origin.x * this.scale / 2;
+        ctx.scale(flipped ? -1 : 1, 1);
+        ctx.drawImage(this.texture, this.currentFrame * this.frameWidth, 0, this.frameWidth, this.texture.height, x, y - origin.y * this.scale, this.frameWidth * this.scale, this.texture.height * this.scale);
+
+        ctx.restore();
+    }
+}
+
+class GameObjects {
+    animations = {};
+    currentAnimation = '';
+    flipped = false;
+    position = new Vector2();
+    rectangle = new Rectangle();
+    origin = new Vector2();
+    scale = 1;
+
+    updateAnimation() {
+        if (this.animations[this.currentAnimation]) {
+            if (this.animations[this.currentAnimation].finishedPlaying && this.animations[this.currentAnimation].nextAnimation != '') {
+                this.playAnimation(this.animations[this.currentAnimation].nextAnimation);
+            }
+            else {
+                this.animations[this.currentAnimation].update();
+            }
+        }
+    }
+
+    playAnimation(animation) {
+        if (animation.length > 0 && this.animations[animation]) {
+            this.currentAnimation = animation;
+            this.animations[animation].play();
+            this.scale = this.animations[animation].scale;
+        }
+    }
+
+    update() {
+        this.updateAnimation();
+    }
+
+    draw() {
+        if (this.currentAnimation.length > 0 && this.animations[this.currentAnimation]) {
+            this.animations[this.currentAnimation].draw(this.position.x, this.position.y, this.flipped, this.origin);
+        }
+    }
+}
+
+class Game {
+    constructor() {
+        this.backgroundColor = '#000000';
+        this.sounds = {};
+    }
+
+    run() {
+        this.preload();
+        setInterval(() => { this.update(); this.drawLoop(); }, 1000 / 60);
+    }
+
+    preload() {
+        
+    }
+
+    update() {
+        
+    }
+
+    clear() {
+        ctx.fillStyle = this.backgroundColor;
+        ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    }
+
+    drawLoop() {
+        this.clear();
+
+        ctx.beginPath();
+        this.draw();
+        ctx.closePath();
+    }
+
+    draw() {
+
+    }
+}
---
+++ game.js
@@ -0,0 +1,235 @@
+class Enemy extends GameObjects {
+    constructor(x, y, flipped) {
+        super();
+
+        this.flipped = flipped;
+
+        this.position = new Vector2(x, y);
+
+        var enemyWalk = new Image();
+        enemyWalk.src = './EnemyWalk.png';
+
+        var enemyAttack = new Image();
+        enemyAttack.src = './EnemyAttack.png';
+
+        var enemyIdle = new Image();
+        enemyIdle.src = './EnemyIdle.png';
+
+        var enemyDie = new Image();
+        enemyDie.src = './EnemyDie.png';
+
+        this.scale = 2;
+
+        this.animations.idle = new Animation(enemyIdle, 88, 4, true, this.scale);
+        this.animations.walk = new Animation(enemyWalk, 88, 3, true, this.scale);
+        this.animations.attack = new Animation(enemyAttack, 88, 5, false, this.scale);
+        this.animations.attack.nextAnimation = 'idle';
+        this.animations.die = new Animation(enemyDie, 88, 4, false, this.scale);
+        this.playAnimation('walk');
+
+        this.alive = true;
+        this.speed = new Vector2(2, 0);
+
+        this.canPlayAttackSound = true;
+    }
+
+    update() {
+        if (this.currentAnimation === 'walk') {
+            if (this.flipped) {
+                this.position.x -= this.speed.x;
+            }
+            else {
+                this.position.x += this.speed.x;
+            }
+        }
+
+        if (this.position.x < -200 || this.position.x > canvas.width + 200) {
+            this.alive = false;
+        }
+
+        this.rectangle = new Rectangle( this.flipped ? this.position.x - 34 * this.scale : this.position.x + 22 * this.scale, this.position.y + 12 * this.scale, 10 * this.animations[this.currentAnimation].scale, 18 * this.animations[this.currentAnimation].scale);
+        if (Rectangle.intersects(game.player.swordRect, this.rectangle)) {
+            if (this.currentAnimation !== 'die') game.sounds.enemyHit.play();
+            this.playAnimation('die'); 
+        }
+
+        if (Rectangle.intersects(game.player.enemyArea, this.rectangle)) {
+            if (this.currentAnimation === 'walk') this.playAnimation('attack'); 
+        }
+
+        if (this.currentAnimation === 'attack' && this.animations.attack.currentFrame === 2 && this.canPlayAttackSound && game.player.currentAnimation !== 'die') {
+            game.sounds.enemySwing.play();
+            this.canPlayAttackSound = false;
+        }
+
+        if (this.currentAnimation === 'attack' && this.animations.attack.currentFrame >= 6) {
+            game.player.alive = false;
+        }
+
+        if (!game.player.alive && this.currentAnimation === 'walk') {
+            this.playAnimation('idle');
+        }
+
+        if (this.animations.die.finishedPlaying) this.alive = false;
+
+        super.update();
+    }
+
+    draw() {
+        Primitives2D.fillRectangle(this.rectangle.x, this.rectangle.y, this.rectangle.width, this.rectangle.height, '#FF000044');
+        super.draw();
+    }
+}
+
+class EnemyGenerator {
+    constructor() {
+        this.enemies = [];
+        this.enemyTimer = 0;
+        this.enemyDelay = 20;
+
+        this.lastSide = 'left';
+    }
+
+    addEnemy() {
+        var shouldCreateEnemy = Math.random() > 0.65;
+
+        if (shouldCreateEnemy) {
+            var side = this.lastSide === 'left' ? 'right' : 'left';
+            var enemy = new Enemy(side === 'left' ? -150 : canvas.width + 150, canvas.height - 96, side === 'right');
+            this.enemies.push(enemy);
+            this.lastSide = side;
+        }
+    }
+
+    update() {
+        this.enemies.forEach((enemy) => {
+            enemy.update();
+
+            if (!enemy.alive) {
+                this.enemies.splice(this.enemies.indexOf(enemy), 1);
+            }
+        });
+
+        this.enemyTimer++;
+
+        if (this.enemyTimer > this.enemyDelay && this.enemies.length < 4) {
+            this.addEnemy();
+            this.enemyTimer = 0;
+        }
+    }
+
+    draw() {
+        this.enemies.forEach((enemy) => {
+            enemy.draw();
+        });
+    }
+}
+
+class Player extends GameObjects {
+    constructor(x, y) {
+        super();
+
+        this.scale = 2;
+
+        this.position = new Vector2(x, y);
+        this.flipped = true;
+
+        this.alive = true;
+
+        var playerAttack = new Image();
+        playerAttack.src = './PlayerAttack.png';
+
+        var playerIdle = new Image();
+        playerIdle.src = './PlayerIdle.png';
+
+        var playerDie = new Image();
+        playerDie.src = './PlayerDie.png';
+
+        this.keyboardState = new KeyboardState();
+
+        this.animations.idle = new Animation(playerIdle, 48, 4, true, this.scale);
+        this.animations.attack = new Animation(playerAttack, 48, 4, false, this.scale, 'idle');
+        this.animations.die = new Animation(playerDie, 48, 4, false, this.scale);
+
+        this.swordRect = new Rectangle();
+        this.origin = new Vector2(48, 24);
+
+        this.enemyArea = new Rectangle(this.position.x - 20 * this.scale, this.position.y - 64 * this.scale, 40 * this.scale, 64 * this.scale);
+
+        this.playAnimation('idle');
+
+        this.canPlayBloodSound = true;
+    }
+
+    update() {
+        if (!this.alive && this.currentAnimation !== 'die') {
+            game.sounds.enemyAttack.play();
+            this.playAnimation('die');
+            super.update();
+
+            return;
+        }
+        
+        if (this.currentAnimation === 'idle') {
+            if (this.animations.idle.currentFrame === 3 && this.canPlayBloodSound) {
+                game.sounds.playerBlood.play();
+                this.canPlayBloodSound = false;
+            }
+            else if (this.animations.idle.currentFrame > 3) {
+                this.canPlayBloodSound = true;
+            }
+        }
+
+        if (this.keyboardState.isKeyDown('Space') && ( this.currentAnimation === 'idle' || ( this.currentAnimation === 'attack' && this.animations.attack.currentFrame > 8 ) )) {
+            this.playAnimation('attack');
+            this.flipped = !this.flipped;
+
+            game.sounds.playerAttack.play();
+        }
+
+        if (this.currentAnimation === 'attack' && this.animations.attack.currentFrame === 3) {
+            this.swordRect = new Rectangle(this.position.x + (this.flipped ? -20 * this.scale : 12 * this.scale), this.position.y - this.origin.y * this.scale, 24, 64 * this.scale);
+        }
+        else {
+            this.swordRect = new Rectangle();
+        }
+
+        super.update();
+    }
+
+    draw() {
+        Primitives2D.fillRectangle(this.swordRect.x, this.swordRect.y, this.swordRect.width, this.swordRect.height, '#FF000044');
+        Primitives2D.fillRectangle(this.enemyArea.x, this.enemyArea.y, this.enemyArea.width, this.enemyArea.height, '#FFFFFF44');
+        super.draw();
+    }
+}
+
+var game = new Game();
+
+game.preload = function() {
+    this.enemies = new EnemyGenerator();
+    this.player = new Player(150, canvas.height - 50);
+
+    this.sounds.playerAttack = new Howl({ src: ['PlayerAttack.mp3'] });
+    this.sounds.enemyHit = new Howl({ src: ['EnemyHit.mp3'] });
+    this.sounds.enemySwing = new Howl({ src: ['EnemySwing.mp3'] });
+    this.sounds.enemyAttack = new Howl({ src: ['EnemyAttack.mp3'] });
+    this.sounds.playerBlood = new Howl({ src: ['Blood.mp3'], volume: 0.1 });
+
+    this.backgroundColor = '#222222';
+
+    console.log(canvas.width, canvas.height);
+}
+
+game.update = function() {
+    this.enemies.update();
+    this.player.update();
+}
+
+game.draw = function() {
+    this.enemies.draw();
+    this.player.draw();
+    Primitives2D.fillRectangle(0, 650, canvas.width, 650, '#00000066', 4);
+}
+
+game.run();
